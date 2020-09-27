501a9882eba2a4a96afe7612da2bc03b84d837b3
Rey Joseph Samonte <rj.samonte@godigitalcorp.com>
2020-09-27 13:17:17 +0800

    - - added service data and migrations in Infrastracture library

---
+++ Infrastracture/Migrations/20200927050838_InitialCreate.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Infrastracture.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UserData",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    FirstName = table.Column<string>(nullable: true),
+                    LastName = table.Column<string>(nullable: true),
+                    Address = table.Column<string>(nullable: true),
+                    DateCreated = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserData", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserData");
+        }
+    }
+}
---
+++ Infrastracture/Services/UserService.cs
@@ -0,0 +1,105 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Update;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http.Headers;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+using WebAPIExerciseGDC.Data;
+
+
+namespace WebAPIExerciseGDC.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly IMapper _mapper;
+        private readonly DataContext _dataContext;
+        public UserService(IMapper mapper, DataContext dataContext)
+        {
+            _mapper = mapper;
+            _dataContext = dataContext;
+        }
+
+        private static List<UserDataModel> users = new List<UserDataModel>()
+        {
+            new UserDataModel {Id = 0, FirstName="RJ", LastName="Samonte"}
+        };
+
+        public async Task<ServiceResponse<List<GetUserDataDto>>> GetAllUserDetails()
+        {
+            ServiceResponse<List<GetUserDataDto>> serviceResponse = new ServiceResponse<List<GetUserDataDto>>();
+
+            List<UserDataModel> dbUsers = await _dataContext.UserData.ToListAsync();
+            serviceResponse.Data = (dbUsers.Select(u => _mapper.Map<GetUserDataDto>(u))).ToList();
+
+            return serviceResponse;
+        }
+
+        public async Task<ServiceResponse<GetUserDataDto>> GetUserDetailsById(int id)
+        {
+            ServiceResponse<GetUserDataDto> serviceResponse = new ServiceResponse<GetUserDataDto>();
+            UserDataModel dbUser = await _dataContext.UserData.FirstOrDefaultAsync(x => x.Id == id);
+            serviceResponse.Data = _mapper.Map<GetUserDataDto>(dbUser);
+
+            return serviceResponse;
+        }
+
+        public async Task<ServiceResponse<List<GetUserDataDto>>> AddNewUser(AddUserDataDto userDetailsModel)
+        {
+            ServiceResponse<List<GetUserDataDto>> serviceResponse = new ServiceResponse<List<GetUserDataDto>>();
+
+            UserDataModel user = _mapper.Map<UserDataModel>(userDetailsModel);
+
+            //user.Id = users.Max(x => x.Id) + 1;
+            await _dataContext.UserData.AddAsync(user);
+            await _dataContext.SaveChangesAsync();
+
+            serviceResponse.Data = _dataContext.UserData.Select(u => _mapper.Map<GetUserDataDto>(u)).ToList();
+
+            return serviceResponse;
+        }
+
+        public async Task<ServiceResponse<GetUserDataDto>> UpdateUserDetails(UpdateUserDataDto updateUserDetailsDto)
+        {
+            ServiceResponse<GetUserDataDto> serviceResponse = new ServiceResponse<GetUserDataDto>();
+
+            try
+            {
+               var res = _mapper.Map<UpdateUserDataDto, UserDataModel>(updateUserDetailsDto);
+                 _dataContext.UserData.Update(res);
+                await _dataContext.SaveChangesAsync();
+                serviceResponse.Data = _mapper.Map<GetUserDataDto>(res);
+            }
+            catch (Exception ex)
+            {
+                serviceResponse.IsSuccess = false;
+                serviceResponse.Message = ex.Message;
+            }
+
+            return serviceResponse;
+        }
+
+        public async Task<ServiceResponse<List<GetUserDataDto>>> DeleteUserDetails(int id)
+        {
+            ServiceResponse<List<GetUserDataDto>> serviceResponse = new ServiceResponse<List<GetUserDataDto>>();
+            try
+            {
+                UserDataModel user = await _dataContext.UserData.FirstAsync(x => x.Id == id);
+                _dataContext.UserData.Remove(user);
+                await _dataContext.SaveChangesAsync();
+
+                serviceResponse.Data = (_dataContext.UserData.Select(x => _mapper.Map<GetUserDataDto>(x))).ToList();
+            }
+            catch (Exception ex)
+            {
+                serviceResponse.IsSuccess = false;
+                serviceResponse.Message = ex.Message;
+            }
+
+            return serviceResponse;
+        }
+    }
+}
