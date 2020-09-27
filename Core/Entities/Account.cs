687cc448d8294f71fdc2a473679e68e8cf0cc588
Rey Joseph Samonte <rj.samonte@godigitalcorp.com>
2020-09-27 13:16:54 +0800

    - - added Entities and Interfaces in Core library

---
+++ Core/Entities/Account.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Entities
+{
+  public  class Account
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string DateCreated { get; set; } = DateTime.Now.ToShortDateString();
+    }
+}
---
+++ Core/Interfaces/IUserService.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace WebAPIExerciseGDC.Services
+{
+    public interface IUserService
+    {
+        Task<ServiceResponse<List<GetUserDataDto>>> GetAllUserDetails();
+        Task<ServiceResponse<GetUserDataDto>> GetUserDetailsById(int id);
+        Task<ServiceResponse<List<GetUserDataDto>>> AddNewUser(AddUserDataDto userDetailsModel);
+        Task<ServiceResponse<GetUserDataDto>> UpdateUserDetails(UpdateUserDataDto userDetailsModel);
+        Task<ServiceResponse<List<GetUserDataDto>>> DeleteUserDetails(int id);
+    }
+}
